4dd4cadcfcb8673f822f3b800e2cfce1781c807a
ingaroca26 <ingaroca26@outlook.com>
2022-01-23 20:17:12 -0500

    v0.0.2

--- helloworld/hello_world.py
+++ helloworld/hello_world.py
@@ -7,3 +7,14 @@ def return_capitalized_hello() -> str:
 def return_world() -> str:
     world: str = 'world'
     return world
+
+
+def print_hello_world() -> None:
+    capitalized_hello: str = return_capitalized_hello()
+    world: str = return_world()
+    hello_world: str = f'{capitalized_hello}, {world}!'
+    print(hello_world)
+
+
+if __name__ == '__main__':
+    print_hello_world()
