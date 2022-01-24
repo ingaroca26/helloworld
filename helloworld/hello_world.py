39182e0c933c275631be2c28736e83bec4d1d42e
ingaroca26 <ingaroca26@outlook.com>
2022-01-23 20:02:52 -0500

    v0.0.1

---
+++ helloworld/hello_world.py
@@ -0,0 +1,9 @@
+def return_capitalized_hello() -> str:
+    hello: str = 'hello'
+    capitalized_hello: str = hello.capitalize()
+    return capitalized_hello
+
+
+def return_world() -> str:
+    world: str = 'world'
+    return world
